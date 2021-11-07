d9c2480d2beca44fef675b2669e98a07f1ee6e1b
Leonardo Sato <91346925+cc20141@users.noreply.github.com>
2021-11-06 21:34:17 -0300

    Splash implementada.

--- ESAG/app/src/main/java/br/unicamp/esag/MainActivity.java
+++ ESAG/app/src/main/java/br/unicamp/esag/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         btnTeste.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+                Intent intent = new Intent(MainActivity.this, SplashActivity.class);
                 startActivity(intent);
             }
         });
