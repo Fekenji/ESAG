8793b75e1c5a8a30ac3465e2d6c5dd747256771a
Leonardo Sato <91346925+cc20141@users.noreply.github.com>
2021-11-18 16:15:54 -0300

    Splash screen atualizada

--- ESAG/app/src/main/java/br/unicamp/esag/SplashActivity.java
+++ ESAG/app/src/main/java/br/unicamp/esag/SplashActivity.java
@@ -13,6 +13,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+
         //Um tipo de sleep
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -20,6 +21,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 Intent intent = new Intent(SplashActivity.this, CadastroActivity.class);
                 startActivity(intent);;
+                finish();
             }
         }, 2000);
     }
